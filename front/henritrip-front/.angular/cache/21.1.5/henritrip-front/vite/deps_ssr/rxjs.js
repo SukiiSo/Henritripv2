51e96d3594fe3fe6db1849930cf8092bf402c99b
SukiiSo <Joana.iniesta@imerir.com>
2026-02-27 17:22:12 +0100

    remove angular cache

--- front/henritrip-front/.angular/cache/21.1.5/henritrip-front/vite/deps_ssr/rxjs.js
+++
@@ -1,6 +0,0 @@
-import { createRequire } from 'module';const require = createRequire(import.meta.url);
-import {
-  require_cjs
-} from "./chunk-QAEY3IRM.js";
-import "./chunk-YXHVU7DJ.js";
-export default require_cjs();
